60f6fea33dad52e066aed350a1228a0e76f07bee
Vinicius Modesto <vinifragam@gmail.com>
2020-04-11 20:59:34 -0300

    M02: Configurando Babel

---
+++ frontend/module02/public/script.js
@@ -0,0 +1,8 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o browser entenda
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter o código de uma maneira diferente
+ *
+ * Loaders: babel-loader, css-loader, image-loader
+ */
+
+
