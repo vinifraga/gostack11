3a18c47ea6b14af19b0b3cdfcdcc807a35248efe
Vinicius Modesto <vinifragam@gmail.com>
2020-04-11 12:19:35 -0300

    M01: Aplicação Funcional

--- backend/module01/src/index.js
+++ backend/module01/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
@@ -20,34 +21,59 @@ app.use(express.json())
  * Request Body: Conteúdo na hora de criar ou editar um recurso
  */
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query
+  const { title } = request.query
 
-  console.log(title)
-  console.log(owner)
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects
 
-  return response.json(['Projeto 1', 'Projeto 2'])
+  return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body
 
-  console.log(title)
-  console.log(owner)
+  const project = { id: uuid(), title, owner }
 
-  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
+  projects.push(project)
+
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params
+  const { title, owner } = request.body
 
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" })
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(['Projeto 2', 'Projeto 3'])
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(3333, () => {
