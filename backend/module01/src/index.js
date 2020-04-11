81524a3b6c7c761195f6ae91526f51c71c152275
Vinicius Modesto <vinifragam@gmail.com>
2020-04-11 10:55:19 -0300

    M01: Tipos de Parâmetros

--- backend/module01/src/index.js
+++ backend/module01/src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 /**
  * Métodos HTTP:
  * 
@@ -11,15 +13,36 @@ const app = express()
  * DELETE: Deletar uma informação no back-end
  */
 
+/**
+ *
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso
+ */
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query
+
+  console.log(title)
+  console.log(owner)
+
   return response.json(['Projeto 1', 'Projeto 2'])
 })
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body
+
+  console.log(title)
+  console.log(owner)
+
   return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
 })
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  console.log(id)
+
   return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
 })
 
