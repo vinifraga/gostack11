fc97be165c77736fecb0da4bfc9ea9a6c3804daa
Vinicius Modesto <vinifragam@gmail.com>
2020-04-12 14:58:27 -0300

    M03: Criando novos projetos

--- mobile/module03/src/index.js
+++ mobile/module03/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
 
 import api from './services/api'
 
@@ -15,11 +15,20 @@ export default function App() {
 
   useEffect(() => {
     api.get('projects').then(response => {
-      console.log(response.data)
       setProjects(response.data)
     })
   }, [])
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Diego Fernandes"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
+  }
 
   return (
     <>
@@ -37,6 +46,14 @@ export default function App() {
             <Text key={project.id} style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   )
@@ -50,5 +67,17 @@ const styles = StyleSheet.create({
   project: {
     color: "#FFF",
     fontSize: 30,
+  },
+  button: {
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16
   }
 })
