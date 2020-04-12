7362141ed96c7ec05515c5ff9f1ae4b25d52aa29
Vinicius Modesto <vinifragam@gmail.com>
2020-04-12 18:10:38 -0300

    M04: Tipando objetos e vetores

--- typescript/module04/src/routes.ts
+++ typescript/module04/src/routes.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from 'express'
+import createUser from './services/CreateUser'
+
+// string, number, boolean, object, Array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'diego@rocketseat.com.br',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'Javascript', experience: 100 },
+    ]
+  });
+
+  console.log(user.email)
+
   return response.json({ message: "Hello World" })
 }
