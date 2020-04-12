472f0a65afc09a3cf55b181747cde7c534725376
Vinicius Modesto <vinifragam@gmail.com>
2020-04-11 21:32:33 -0300

    M02: Estado e Imutabilidade

--- frontend/module02/src/components/Header.js
+++ frontend/module02/src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export default function Header({ title, children }) {
+export default function Header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
-
-      {children}
     </header>
   )
 }
