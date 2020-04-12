ad21915216d6488a836777886961eda85a1ec43e
Vinicius Modesto <vinifragam@gmail.com>
2020-04-12 13:42:35 -0300

    M02: Cadastrando Projetos

--- frontend/module02/src/App.js
+++ frontend/module02/src/App.js
@@ -24,8 +24,17 @@ function App() {
     })
   }, [])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Diego Fernandes"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
   }
 
   return (
