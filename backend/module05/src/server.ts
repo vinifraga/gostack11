e075e0cc1ae0752c27d88066e28b5c7e600c26d3
Vinicius Modesto <vinifragam@gmail.com>
2020-04-13 13:02:03 -0300

    M05: ESLint

--- backend/module05/src/server.ts
+++ backend/module05/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello GoStack" })
-})
+app.get('/', (request, response) => response.json({ message: 'Hello GoStack' }));
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
