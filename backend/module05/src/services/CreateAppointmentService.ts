ac2d208840fb08516b00b0d8aa719a701cef8a68
Vinicius Modesto <vinifragam@gmail.com>
2020-04-15 15:02:57 -0300

    M05: Services & SOLID

---
+++ backend/module05/src/services/CreateAppointmentService.ts
@@ -0,0 +1,53 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [x] Recebimento das informações
+ * [x] Tratativa de erros/excessões
+ * [x] Acesso ao repositório
+ */
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+/**
+ * SOLID
+ *
+ * Single Responsability Principle
+ * Dependency Inversion
+ */
+
+// DRY: Don't repeat yourself
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
