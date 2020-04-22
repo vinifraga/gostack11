c9f600fb6c06acc14fd555337d179c050590fac9
Vinicius Modesto <vinifragam@gmail.com>
2020-04-22 14:31:13 -0300

    M06: Rotas autenticadas

--- backend/module06/src/routes/appointments.routes.ts
+++ backend/module06/src/routes/appointments.routes.ts
@@ -5,11 +5,15 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticaded from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
 
 // SoC: Separation of Concerns (separação de preocupações)
 // DTO: Data Transfer Object
 // Rotas: Receber a requisição, chamar outro arquivo e devolver uma resposta
+
+appointmentsRouter.use(ensureAuthenticaded);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
--- backend/module06/src/services/AuthenticateUserService.ts
+++ backend/module06/src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +31,11 @@ class AutheticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '177f1a6344df5c4b4acb291e98927e6f', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
