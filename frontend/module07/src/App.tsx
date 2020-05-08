d8acb2b15e82749c59499b8ddff4ef41c5f2f5a3
Vinicius Fraga <vinifragam@gmail.com>
2020-05-08 20:49:26 -0300

    M07: Utilizando Styled Components

--- frontend/module07/src/App.tsx
+++ frontend/module07/src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
