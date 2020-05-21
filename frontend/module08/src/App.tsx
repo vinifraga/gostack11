197a6b58610dea7834aef06c2cd5878a728a8102
Vinicius Fraga <vinifragam@gmail.com>
2020-05-21 17:20:44 -0300

    M08: Estilos globais

--- frontend/module08/src/App.tsx
+++ frontend/module08/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
