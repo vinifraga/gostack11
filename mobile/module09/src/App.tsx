90402f777899a915fbdae5ad67b7eeb08102a328
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 13:00:56 -0300

    M09: Configurando estrutura

---
+++ mobile/module09/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{ 
+      flex: 1,
+      alignItems: "center",
+      justifyContent: 'center',
+     }}>
+      <Text style={{
+        fontSize: 30,
+        fontWeight: 'bold'
+      }}>Módulo 09</Text>
+      <Text style={{
+        fontSize: 18,
+        marginTop: 10
+      }}>Iniciando aplicativo Mobile</Text>
+    </View>
+  );
+};
+
+export default App;
