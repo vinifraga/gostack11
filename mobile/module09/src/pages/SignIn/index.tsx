0c9c30f3e472cadbe5f35bc63d2fe512f0de0380
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 13:51:42 -0300

    M09: Densidade de pixel

--- mobile/module09/src/pages/SignIn/index.tsx
+++ mobile/module09/src/pages/SignIn/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
 
 export default SignIn;
