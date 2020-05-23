a729f9d1a793eaf73bc8e76643304b813959c6a9
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 11:51:02 -0300

    M08: Rotas autenticadas

--- frontend/module08/src/routes/index.tsx
+++ frontend/module08/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Route from './route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
