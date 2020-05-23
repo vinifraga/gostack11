ff39bca57e27e269380c43829a31f14ff3d9cc55
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 13:14:00 -0300

    M09: Padrões de estilo

--- mobile/module09/src/App.tsx
+++ mobile/module09/src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <View style={{ 
-      flex: 1,
-      alignItems: "center",
-      justifyContent: 'center',
-     }}>
-      <Text style={{
-        fontSize: 30,
-        fontWeight: 'bold'
-      }}>Módulo 09</Text>
-      <Text style={{
-        fontSize: 18,
-        marginTop: 10
-      }}>Iniciando aplicativo Mobile</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}
+      >
+        Módulo 09
+      </Text>
+      <Text
+        style={{
+          fontSize: 18,
+          marginTop: 10,
+        }}
+      >
+        Iniciando aplicativo Mobile
+      </Text>
     </View>
   );
 };
