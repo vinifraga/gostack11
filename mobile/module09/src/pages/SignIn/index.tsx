0ef2cfed9d131ae77804a78e0af375abdf642afc
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 22:19:14 -0300

    M09: Input & Button

--- mobile/module09/src/pages/SignIn/index.tsx
+++ mobile/module09/src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -10,6 +13,17 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
