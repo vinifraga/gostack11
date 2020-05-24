1df40151d4f2581adcf6f7473cd4c7f3241360cf
Vinicius Fraga <vinifragam@gmail.com>
2020-05-23 21:41:31 -0300

    M09: Importando fontes externas

--- mobile/module09/src/pages/SignIn/index.tsx
+++ mobile/module09/src/pages/SignIn/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
