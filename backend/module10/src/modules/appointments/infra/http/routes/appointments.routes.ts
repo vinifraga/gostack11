9d3923b84972334cb22c1991ef49cc80417af426
Vinicius Fraga <vinifragam@gmail.com>
2020-05-31 17:02:37 -0300

    M10: Usando controllers

--- backend/module10/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/module10/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 // SoC: Separation of Concerns (separação de preocupações)
 // DTO: Data Transfer Object
@@ -20,19 +18,6 @@ appointmentsRouter.use(ensureAuthenticaded);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
