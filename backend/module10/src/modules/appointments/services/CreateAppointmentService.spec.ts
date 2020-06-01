634140925116ceaefd348c22d8b839668db38dc3
Vinicius Fraga <vinifragam@gmail.com>
2020-06-01 00:25:50 -0300

    M10: Criando o primeiro teste

---
+++ backend/module10/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
--- backend/module10/src/modules/appointments/services/CreteAppointmentService.spec.ts
+++
@@ -1,9 +0,0 @@
-import CreateAppointmentService from './CreateAppointmentService';
-
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {});
-
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
-});
