bfdf133c79e4dcc0efb56817ccc1b08eec1eae23
Vinicius Fraga <vinifragam@gmail.com>
2020-06-15 16:11:49 -0300

    M11: Emails em desenvolvimento

--- backend/module11/src/shared/container/providers/index.ts
+++ backend/module11/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './EmailProvider/models/IMailProvider';
+import EtherealMailProvider from './EmailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
