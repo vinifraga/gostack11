97ec8be4109bda38907ccbcffc8d2dff11f50198
Vinicius Fraga <vinifragam@gmail.com>
2020-07-13 11:24:21 -0300

    M11: Criação do agendamento

--- backend/module11/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/module11/src/modules/appointments/services/CreateAppointmentService.ts
@@ -7,6 +7,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
@@ -17,7 +18,11 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
@@ -30,6 +35,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
