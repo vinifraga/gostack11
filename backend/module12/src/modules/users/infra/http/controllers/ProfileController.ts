729dca39722f7b576cfb990313048d04bedb42fd
Vinicius Fraga <vinifragam@gmail.com>
2020-07-14 02:26:20 -0300

    M12: Utilizando Class Transformer

--- backend/module12/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/module12/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -11,9 +12,7 @@ export default class UsersController {
 
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -30,8 +29,6 @@ export default class UsersController {
       password,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
