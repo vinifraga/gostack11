0a5249f0618c45b6d3d423771c156558c8e1e255
Vinicius Fraga <vinifragam@gmail.com>
2020-07-14 14:49:20 -0300

    M12: Emails pelo Amazon SES

---
+++ backend/module12/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'me@vinifraga.com',
+      name: 'Vini da Rocketseat',
+    },
+  },
+} as IMailConfig;
