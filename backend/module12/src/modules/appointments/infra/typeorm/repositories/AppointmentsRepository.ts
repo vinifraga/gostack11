e412e721550b815f6fbe3217dee04e8654413d77
Vinicius Fraga <vinifragam@gmail.com>
2020-07-18 12:09:58 -0300

    M13: Agendamentos no mesmo horário

--- backend/module12/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/module12/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -29,9 +29,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
     return appointment;
   }
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
     const findAppointment = await this.ormRepository.findOne({
-      where: { date },
+      where: { date, provider_id },
     });
 
     return findAppointment;
