6b6a4753d62eb8ddb735275edef8084310b53482
Vinicius Fraga <vinifragam@gmail.com>
2020-07-18 11:54:06 -0300

    M13: Utilizando Query Params

--- backend/module12/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ backend/module12/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { month, year } = request.body;
+    const { month, year } = request.query;
     const { provider_id } = request.params;
 
     const listProviderMonthAvailability = container.resolve(
@@ -14,8 +14,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
