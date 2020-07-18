786a4312acdaf08361d34b4a9ad97e59a88fbd59
Vinicius Fraga <vinifragam@gmail.com>
2020-07-18 12:48:21 -0300

    M13: Clientes dos agendamentos

--- backend/module12/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/module12/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -77,6 +77,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
