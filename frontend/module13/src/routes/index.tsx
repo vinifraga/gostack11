5a6e64521714257a400ee2d4d9a7a6b882f5f0e8
Vinicius Fraga <vinifragam@gmail.com>
2020-07-19 14:28:59 -0300

    M13: Criando página de redefinição

--- frontend/module13/src/routes/index.tsx
+++ frontend/module13/src/routes/index.tsx
@@ -5,6 +5,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Route from './route';
 
 const Routes: React.FC = () => (
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
     <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
