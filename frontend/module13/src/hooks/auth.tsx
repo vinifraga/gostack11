677773acf72a1882b0cd6a6f7c0cbce0ac669615
Vinicius Fraga <vinifragam@gmail.com>
2020-07-19 16:52:58 -0300

    M13: Disponibilidade do mês

--- frontend/module13/src/hooks/auth.tsx
+++ frontend/module13/src/hooks/auth.tsx
@@ -31,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return {
         token,
         user: JSON.parse(user),
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('GoBarber:token', token);
     localStorage.setItem('GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
