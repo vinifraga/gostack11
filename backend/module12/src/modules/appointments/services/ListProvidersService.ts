be1e0778edff98203e6d3f68bf91d43440b83644
Vinicius Fraga <vinifragam@gmail.com>
2020-07-21 00:31:37 -0300

    M14: Lista de providers com serialização

--- backend/module12/src/modules/appointments/services/ListProvidersService.ts
+++ backend/module12/src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -28,7 +29,7 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
     return users;
