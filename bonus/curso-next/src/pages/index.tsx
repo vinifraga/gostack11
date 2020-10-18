79e5950a325adaebdc65624982a41a3b31b54264
Vinicius Fraga <vinifragam@gmail.com>
2020-10-18 16:33:00 -0300

    Next08: Server Side Rendering

--- bonus/curso-next/src/pages/index.tsx
+++ bonus/curso-next/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { GetServerSideProps } from 'next'
 import { Title } from '../styles/pages/Home'
 
 interface IProduct {
@@ -6,17 +6,11 @@ interface IProduct {
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3333/recommended').then(response => {
-      response.json().then(data => {
-        setRecommendedProducts(data);
-      })
-    })
-  }, [])
-
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <section>
       <Title>Products</Title>
@@ -32,4 +26,15 @@ export default function Home() {
       </ul>
     </section>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended')
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
