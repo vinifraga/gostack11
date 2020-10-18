cd2f201ce168e03137bc62df4e749d2c0ed10fee
Vinicius Fraga <vinifragam@gmail.com>
2020-10-18 17:19:03 -0300

    Next11: Incremental Static Generation

--- bonus/curso-next/src/pages/catalog/categories/[slug].tsx
+++ bonus/curso-next/src/pages/catalog/categories/[slug].tsx
@@ -13,6 +13,10 @@ interface CategoryProps {
 export default function Category({ products }: CategoryProps) {
   const router = useRouter();
 
+  if (router.isFallback) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
       <h1>{router.query.slug}</h1>
@@ -42,7 +46,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
