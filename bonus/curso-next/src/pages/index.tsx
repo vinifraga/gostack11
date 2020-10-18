0ec7d3f7d76acb7ff4347ee6d0c75929065a5493
Vinicius Fraga <vinifragam@gmail.com>
2020-10-18 16:11:47 -0300

    Next05: Styled Components

--- bonus/curso-next/src/pages/index.tsx
+++ bonus/curso-next/src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Title } from '../styles/pages/Home'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   )
 }
