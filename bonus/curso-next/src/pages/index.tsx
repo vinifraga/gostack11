933b393b888101bded91f12ad8f7b4d9f0991be2
Vinicius Fraga <vinifragam@gmail.com>
2020-10-18 16:24:32 -0300

    Next07: Client Side Fetching

--- bonus/curso-next/src/pages/index.tsx
+++ bonus/curso-next/src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react'
 import { Title } from '../styles/pages/Home'
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    })
+  }, [])
+
   return (
-    <div>
-      <Title>Hello World</Title>
-    </div>
+    <section>
+      <Title>Products</Title>
+
+      <ul>
+        {recommendedProducts.map(recommendedProduct => {
+          return (
+            <li key={recommendedProduct.id}>
+              {recommendedProduct.title}
+            </li>
+          )
+        })}
+      </ul>
+    </section>
   )
 }
