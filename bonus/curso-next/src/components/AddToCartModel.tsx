4ba6d5b1f26a4ae5f2b6ecf43efbb4c89ddc4557
Vinicius Fraga <vinifragam@gmail.com>
2020-10-18 17:37:44 -0300

    Next14: Lazy load de componentes

---
+++ bonus/curso-next/src/components/AddToCartModel.tsx
@@ -0,0 +1,7 @@
+export default function AddToCartModal() {
+  return (
+    <div>
+      Do you really want to add this product to the cart?
+    </div>
+  )
+}
--- bonus/curso-next/src/pages/catalog/products/[slug].tsx
+++ bonus/curso-next/src/pages/catalog/products/[slug].tsx
@@ -1,7 +1,27 @@
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { useState } from 'react';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModel'),
+  { loading: () => <p>Loading...</p>, ssr: false }
+)
 
 export default function Product() {
   const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-  return <h1>{router.query.slug}</h1>
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      { isAddToCartModalVisible && <AddToCartModal />}
+    </div>
+  )
 }
