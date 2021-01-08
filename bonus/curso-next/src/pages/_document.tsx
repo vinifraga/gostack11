7b0881a2b87ab68b62cc1c1e730e44a573692805
Vini Fraga <vinifragam@gmail.com>
2021-01-08 13:48:44 -0300

    Next18: Criando documento customizado

--- bonus/curso-next/src/pages/_document.tsx
+++ bonus/curso-next/src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -26,5 +26,23 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+      
+    )
   }
 }
--- bonus/curso-next/src/styles/GlobalStyle.ts
+++ bonus/curso-next/src/styles/GlobalStyle.ts
@@ -9,6 +9,6 @@ export default createGlobalStyle`
   body {
     background: #121214;
     color: #fff;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
   }
 `;
