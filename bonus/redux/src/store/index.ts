143cee43e77f9145499fc17e72995616862f1acc
Vini Fraga <vinifragam@gmail.com>
2021-02-10 23:55:49 -0300

    Redux05: Usando os reducers

--- bonus/redux/src/store/index.ts
+++ bonus/redux/src/store/index.ts
@@ -1,5 +1,7 @@
 import { createStore } from 'redux';
 
-const store = createStore(() => [])
+import rootReducer from './modules/rootReducer';
+
+const store = createStore(rootReducer)
 
 export default store;
