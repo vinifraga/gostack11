fec09b1a4cb6d06b2857492618c02f1773f96c93
Vini Fraga <vinifragam@gmail.com>
2021-02-11 00:09:06 -0300

    Redux07: Criando servidor JSON

--- bonus/redux/src/components/Catalog.tsx
+++ bonus/redux/src/components/Catalog.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import { IProduct } from '../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state);
-
-  console.log(catalog)
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
   
+  useEffect(() => {
+    api.get('/products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
+
   return (
+    <main>
     <h1>Catalog</h1>
+
+    {catalog.map(product => (
+      <article key={product.id}>
+        <strong>{product.title}</strong> {" - "}
+        <span>{product.price}</span> {" "}
+
+        <button type="button">Comprar</button>
+      </article>
+    ))}
+    </main>
   );
 }
 
