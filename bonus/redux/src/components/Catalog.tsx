5053fbaa0ab035eb460d721d6bfdfcd54107b7c7
Vini Fraga <vinifragam@gmail.com>
2021-02-11 23:20:23 -0300

    Redux14: Abstraindo item do catálogo

--- bonus/redux/src/components/Catalog.tsx
+++ bonus/redux/src/components/Catalog.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
   
   useEffect(() => {
@@ -15,22 +13,11 @@ const Catalog: React.FC = () => {
     })
   }, [])
 
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, [dispatch])
-
   return (
     <main>
     <h1>Catalog</h1>
 
-    {catalog.map(product => (
-      <article key={product.id}>
-        <strong>{product.title}</strong> {" - "}
-        <span>{product.price}</span> {" "}
-
-        <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
-      </article>
-    ))}
+    {catalog.map(product => <CatalogItem key={product.id} product={product} />)}
     </main>
   );
 }
