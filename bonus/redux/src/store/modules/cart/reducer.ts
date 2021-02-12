add05f0ced619e1f950baaeab6a004b31e6dbf2e
Vini Fraga <vinifragam@gmail.com>
2021-02-11 21:52:27 -0300

    Redux09: Alterando estado

--- bonus/redux/src/store/modules/cart/reducer.ts
+++ bonus/redux/src/store/modules/cart/reducer.ts
@@ -5,10 +5,26 @@ const INITIAL_STATE: ICartState = {
   items: []
 };
 
-const cart: Reducer<ICartState> = (state, action) => {
-  console.log(state, action)
-  
-  return INITIAL_STATE
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case 'ADD_PRODUCT_TO_CART': {
+      const { product } = action.payload
+      
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            product,
+            quantity: 1
+          }
+        ]
+      }
+    }
+    default: {
+      return state
+    }
+  }
 }
 
 export default cart;
