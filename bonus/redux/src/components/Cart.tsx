b4cf2a5fdbc1076b8cf78f6976f8567da1ffe69a
Vini Fraga <vinifragam@gmail.com>
2021-02-11 22:13:16 -0300

    Redux10: Tipagens do Redux

--- bonus/redux/src/components/Cart.tsx
+++ bonus/redux/src/components/Cart.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { IState } from '../store';
+import { ICartItem } from '../store/modules/cart/types';
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
-  console.log(state)
+  console.log(cart)
 
   return (
     <table>
@@ -16,6 +18,16 @@ const Cart: React.FC = () => {
           <th>Subtotal</th>
         </tr>
       </thead>
+      <tbody>
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
--- bonus/redux/src/store/index.ts
+++ bonus/redux/src/store/index.ts
@@ -2,6 +2,12 @@ import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
+import { ICartState } from './modules/cart/types';
+
+export interface IState {
+  cart: ICartState
+}
+
 const store = createStore(rootReducer)
 
 export default store;
