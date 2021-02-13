5a0ba7318c54181c84849363de0ae7a72e8d10ed
Vini Fraga <vinifragam@gmail.com>
2021-02-12 21:37:53 -0300

    Redux16: Obtendo quantidade do carrinho

--- bonus/redux/src/store/modules/cart/sagas.ts
+++ bonus/redux/src/store/modules/cart/sagas.ts
@@ -1,6 +1,19 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, takeLatest, select } from 'redux-saga/effects'
+import { addProductToCart } from './actions'
 
-function checkProductStock() {
+import { IState } from '../..';
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
+
   console.log('adicionou ao carrinho')
 }
 
